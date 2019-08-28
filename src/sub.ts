89da31abf6bfb97758a92fc19ef8531116519124
Asari Mitsuteru <asari.mtr@gmail.com>
2019-08-28 17:15:35 +0900

    tweaks and lint --fix

--- src/sub.ts
+++ src/sub.ts
@@ -2,9 +2,6 @@ export const hello = (message: string): void => {
   log(message);
 };
 
-function log(message: string) :void {
+function log(message: string): void {
   console.log(`${message}を出力しました!`);
-}
-
-export function piyo() {
 }
